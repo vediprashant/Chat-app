8973b2686864b236e05ce4d2f479327ff3459158
vediprashant <42546403+vediprashant@users.noreply.github.com>
2019-09-19 00:52:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.port || 5000
+var port = process.env.PORT || 5000
 
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
