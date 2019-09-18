644145c8e6941a83cf7f2ea96f0c8ab43a5c25a4
vediprashant <42546403+vediprashant@users.noreply.github.com>
2019-09-18 19:27:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 // var server = require('http').Server(app);
 var io = require('socket.io')(3000);
 
